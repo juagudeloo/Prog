9a388181f22363d252031d1ccef205dc79904e93
Juan Estean Agudelo Ortiz <juagudeloo@darkstar.org>
2019-09-06 08:47:39 -0500

    segundo commit después del de el ejemplo

---
+++ 2019-09-04/leer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void)
+{
+  double x = 0.0;
+  
+  std::cout <<"Escribe el número :" << std::endl;
+  std::cin >> x;
+  std::cout << " He leido : " << x << std::endl;
+  return 0;
+}
