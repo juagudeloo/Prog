dea4fa85feec4c8ee19e8fb39755e5422d6e88d8
juagudeloo <juagudeloo@unal.edu.co>
2019-11-02 18:38:58 -0500

    prueba del heap del computador de mesa de mi casa en Cogua (el grande)

---
+++ EJERCICIOS/tareas/pruebaheap.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+int main (void)
+{
+  const int N = 680000000;//it gets killed in 681000000
+  std::vector <int> tamano (N);
+  for (int ii = 0; ii < N; ii++){
+    tamano[ii] = 0;
+      }
+  std::cout << "0" << std::endl;
+}
